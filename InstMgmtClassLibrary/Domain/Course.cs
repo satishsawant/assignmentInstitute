ba970a71c11e2e2d25b6cdd5aa60be0679f2c351
bhagyashri wakade <bhagyashri4994@gmail.com>
2018-06-21 11:05:30 +0000

    Domain Classes created

---
+++ InstMgmtClassLibrary/Domain/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstMgmtClassLibrary.Domain
+{
+    class Course
+    {
+        public int CourseId { get; set; }
+
+        public int CourseTypeId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Duration { get; set; }
+
+        public int Fees { get; set; }
+
+    }
+}
