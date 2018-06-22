58bd69eaf24baa509015001a8ade668889f5d65b
bhagyashri wakade <bhagyashri4994@gmail.com>
2018-06-22 13:50:12 +0000

    ui changes

---
+++ InstituteManagement/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstituteManagement.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()       
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+        public ActionResult Find()
+        {
+            return View();
+        }
+    }
+}
