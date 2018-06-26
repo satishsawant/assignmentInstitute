b70dd46461dd882e2437035ddfe35614bfedff53
bhagyashri wakade <bhagyashri4994@gmail.com>
2018-06-26 05:08:13 +0000

    All UI changes

--- InstituteManagement/App_Start/BundleConfig.cs
+++ InstituteManagement/App_Start/BundleConfig.cs
@@ -21,11 +21,14 @@ namespace InstituteManagement
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js"
+                     ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.sandstone.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-flatly.css",
+                      //"~/Content/bootstrap.css",
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
---
+++ InstituteManagement/Controllers/ExpenseTypeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstituteManagement.Controllers
+{
+    public class ExpenseTypeController : Controller
+    {
+        // GET: Expense
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ExpenseTypeCreate()
+        {
+            return View();
+        }
+        public ActionResult ExpenseTypeList()
+        {
+            return View();
+        }
+    }
+}
